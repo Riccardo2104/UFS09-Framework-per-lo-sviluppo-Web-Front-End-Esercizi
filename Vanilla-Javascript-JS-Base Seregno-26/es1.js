6a7fc42e7496fdc3364d5d30a7e91ffe2a815f9c
Riccardo <riccardovigano21@gmail.com>
2026-04-29 16:30:01 +0200

    primo esercizio

---
+++ Vanilla-Javascript-JS-Base Seregno-26/es1.js
@@ -0,0 +1,13 @@
+/*
+Aprire la console (in basso a destra)
+Creare una variabile "nome" usando la parola chiave "const" ed assegnare il testo "Renzo"
+stampare il valore nella console usando il comando "console.log"
+modificare successivamente il valore di "nome" in "Lucia"
+stampare il valore di "nome"
+Cosa succede nella console?
+
+*/
+let nome = "Renzo";
+console.log(nome);
+nome = "lucia";
+console.log(nome);
